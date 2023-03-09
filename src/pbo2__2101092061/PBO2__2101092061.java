e17c066ace63365cba068d342767efed62f279ea
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-03-09 16:04:47 +0700

    Update PBO2__2101092061.java

--- src/pbo2__2101092061/PBO2__2101092061.java
+++ src/pbo2__2101092061/PBO2__2101092061.java
@@ -5,6 +5,13 @@
  */
 package pbo2__2101092061;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import nella.dao.Koneksi;
+
 /**
  *
  * @author Asus
@@ -15,7 +22,13 @@ public class PBO2__2101092061 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        try {
+            Koneksi koneksi = new Koneksi();
+            Connection con = koneksi.getkoneksi();
+            JOptionPane.showMessageDialog(null,"Koneksi OK");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null,"Error :" +ex.getMessage());
+            Logger.getLogger(PBO2__2101092061.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }    
 }
