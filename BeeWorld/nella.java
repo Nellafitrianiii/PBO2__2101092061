be77d8929dda752f6f3545e4e638e1c08dc81ab9
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-05-17 10:28:43 +0700

    Create nella.java

---
+++ BeeWorld/nella.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class nella here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class nella extends Actor
+{
+    /**
+     * Act - do whatever the nella wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
