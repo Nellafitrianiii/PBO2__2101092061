bbfb85e3af35c6ce79b14db456fe6b0b5efc9ad2
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-05-17 10:30:12 +0700

    Update AnggotaDao.java

--- src/nella/dao/AnggotaDao.java
+++ src/nella/dao/AnggotaDao.java
@@ -6,6 +6,8 @@
 package nella.dao;
 
 import java.sql.Connection;
+import java.util.List;
+
 import nella.model.Anggota;
 
 /**
@@ -13,5 +15,10 @@ import nella.model.Anggota;
  * @author Asus
  */
 public interface AnggotaDao {
-    void insert (Connection con, Anggota anggota) throws Exception;
+    public void insert (Connection con, Anggota anggota) throws Exception;
+    public void update (Connection con, Anggota anggota) throws Exception;
+    public void delete (Connection con, Anggota anggota) throws Exception;
+    public Anggota getAnggota (Connection con, String kode) throws Exception;
+    public List<Anggota> getAllAnggota(Connection con) throws Exception;
+    
 }
