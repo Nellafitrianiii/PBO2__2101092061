a1cd43e1e63ced27f922a156ae1d30a11a45dfb2
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-05-17 12:56:50 +0700

    Create PlnDaoImpl.java

---
+++ src/uts_a/dao/PlnDaoImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_a.dao;
+
+import java.sql.Connection;
+import java.util.List;
+import uts_a.model.Pln;
+
+/**
+ *
+ * @author Asus
+ */
+public class PlnDaoImpl implements PlnDao {
+
+    @Override
+    public void insert(Connection con, Pln pln) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Connection con, Pln pln) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Connection con, Pln pln) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Pln getPln(Connection con, String kode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Pln> getAllPln(Connection con) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
