738b0988253f207236a5f454df9b8c4660d9f9b8
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-05-17 10:30:09 +0700

    Update AnggotaController.java

--- src/nella/controller/AnggotaController.java
+++ src/nella/controller/AnggotaController.java
@@ -7,8 +7,11 @@ package nella.controller;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import nella.dao.AnggotaDao;
 import nella.dao.AnggotaDaoImpl;
 import nella.dao.Koneksi;
@@ -38,9 +41,89 @@ public class AnggotaController {
         }
     }
     
-    public void bersihForm(){
+    public void insert(){
+        try {
+            anggota = new Anggota ();
+            anggota.setKodeAnggota(
+                    formAnggota.getTxtKodeAnggota().getText());
+            anggota.setNamaAnggota(
+                    formAnggota.getTxtNama().getText());
+            anggota.setAlamat(
+                    formAnggota.getTxtAlamat().getText());
+            anggota.setJenisKelamin (
+                    formAnggota.getCboJenisKelamin().getSelectedItem().toString());
+            anggotaDao.insert(con,anggota);
+            JOptionPane.showMessageDialog(formAnggota, "Entri Ok");
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void Update (){
+        try {
+            anggota = new Anggota();
+            anggota.setKodeAnggota(
+                    formAnggota.getTxtKodeAnggota().getText());
+            anggota.setNamaAnggota(
+                    formAnggota.getTxtNama().getText());
+            anggota.setAlamat(
+                    formAnggota.getTxtAlamat().getText());
+            anggota.setJenisKelamin (
+                    formAnggota.getCboJenisKelamin().getSelectedItem().toString());
+            anggotaDao.insert(con,anggota);
+            JOptionPane.showMessageDialog(formAnggota, "Update Ok"); 
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void delete(){
+        try {
+            anggotaDao.delete(con, anggota);
+            JOptionPane.showMessageDialog(formAnggota,"Delete Ok");
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void cari(){
+        try {
+            String kode = formAnggota.getTxtKodeAnggota().getText();
+            anggota = anggotaDao.getAnggota(con, kode);
+            if(anggota != null){
+                formAnggota.getTxtNama().setText(anggota.getNamaAnggota());
+                formAnggota.getTxtAlamat().setText(anggota.getAlamat());
+                formAnggota.getCboJenisKelamin().setSelectedItem(anggota.getJenisKelamin());
+            }else{
+                JOptionPane.showMessageDialog(formAnggota,"Data Tidak Ada");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void tampil(){
+        try {
+            DefaultTableModel tabel = (DefaultTableModel) formAnggota.getTblAnggota().getModel();
+            tabel.setRowCount(0);
+            List<Anggota> list = anggotaDao.getAllAnggota(con);
+            for (Anggota anggota1 : list){
+                Object[] row = {
+                    anggota1.getKodeAnggota(),
+                    anggota1.getNamaAnggota(),
+                    anggota1.getJenisKelamin()
+                };
+                tabel.addRow(row);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    public void clearForm(){
         formAnggota.getTxtKodeAnggota().setText("");
         formAnggota.getTxtNama().setText("");
+        formAnggota.getTxtAlamat().setText("");
     }
     
     public void isiCboJenisKelamin(){
