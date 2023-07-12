8e8a4e7eadedb93fc35af948b35ab67730e62233
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-07-12 22:17:51 +0700

    Update PlnController.java

--- src/uts_a/controller/PlnController.java
+++ src/uts_a/controller/PlnController.java
@@ -1,105 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package uts_a.controller;
+class Hotel:
+    def __init__(self, name, location, rating, rooms_available):
+        self.name = name
+        self.location = location
+        self.rating = rating
+        self.rooms_available = rooms_available
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
-import uts_a.dao.Koneksi;
-import uts_a.dao.PlnDao;
-import uts_a.dao.PlnDaoImpl;
-import uts_a.view.FormPLN;
-import uts_a.model.Pln;
+    def display_info(self):
+        print("Hotel Name:", self.name)
+        print("Location:", self.location)
+        print("Rating:", self.rating)
+        print("Rooms Available:", self.rooms_available)
 
-/**
- *
- * @author Asus
- */
-public class PlnController {
-    private FormPLN formpln;
-    private Pln pln;
-    private PlnDao plndao;
-    private Connection con;
-    private Koneksi koneksi;
+    def book_room(self, num_rooms):
+        if num_rooms <= self.rooms_available:
+            self.rooms_available -= num_rooms
+            print("Successfully booked", num_rooms, "room(s).")
+        else:
+            print("Sorry, no enough rooms available.")
 
-    public PlnController(FormPLN formpln) throws Exception {
-        this.formpln = formpln;
-        plndao = new PlnDaoImpl;
-        try {
-            con = new Koneksi().getkoneksi();
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(PlnController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public void insert(){
-        pln = new Pln();
-            pln.setKodepelanggan(formpln.getTxtKode().getText());
-            pln.setNampelanggan(formpln.getTxtNama().getText());
-            pln.setMbi(formpln.getTxtMbi().getText());
-            pln.setMbl(formpln.getTxtMbl().getText());
-            pln.setTarif(formpln.getTxtTarif()).getDouble());
-            plndao.insert(con,pln);
-            JOptionPane.showMessageDialog(formpln, "Entri Ok");
-    }
-    
-    public void update(){
-         pln = new Pln();
-            pln.setKodepelanggan(formpln.getTxtKode().getText());
-            pln.setNampelanggan(formpln.getTxtNama().getText());
-            pln.setMbi(formpln.getTxtMbi().getText());
-            pln.setMbl(formpln.getTxtMbl().getText());
-            pln.setTarif(formpln.getTxtTarif()).getDouble());
-            plndao.insert(con,pln);
-            JOptionPane.showMessageDialog(formpln, "Update Ok");
-        
-    }
-    
-    public void delete(){
-        plndao.delete(con,pln);
-        JOptionPane.showMessageDialog(formpln,"Delete Ok");
-    }
-    
-    public void clearForm(){
-        formpln.getTxtKode().setText("");
-        formpln.getTxtNama().setText("");
-        formpln.getTxtMbi().setText("");
-        formpln.getTxtMbl().setText("");
-        formpln.getTxtTarif().setText("");
-    }
-    
-    
-    
-    public void tampil(){
-        try {
-            DefaultTableModel tabel = (DefaultTableModel) formpln.getTblPln().getModel();
-            tabel.setRowCount(0);
-            List<Pln> list = plndao.getAllPln(con);
-            for (pln pln1 : list){
-                Object[] row = {
-                    pln1.getKode(),
-                    pln1.getNama(),
-                    pln1.getMbi(),
-                    pln1.getMbl(),
-                    pln1.getTarif()
-                    
-                };
-                tabel.addRow(row);
-            }
-        
-
-    public void clearForm() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
-    
-    
-}
+# Contoh penggunaan class Hotel
+hotel = Hotel("Grand Hotel", "City Center", 4.5, 100)
+hotel.display_info()  # Menampilkan informasi hotel
+hotel.book_room(3)    # Mencoba memesan 3 kamar
+hotel.display_info()  # Menampilkan informasi hotel setelah memesan kamar
